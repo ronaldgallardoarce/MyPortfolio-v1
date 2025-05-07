fa7607ea814fee53767fecc1e4d5457611bc0f7c
DESKTOP-2J1P6GT <ronaldgallardoarce78@gmail.com>
2025-05-07 10:31:01 -0400

    fix: corrige el error en layout

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,7 +2,6 @@
 import { Outlet } from "react-router";
 import Header from "../components/header";
 import ThemeGradientBackground from "../components/ui/themeGradientBackground";
-import Footer from "../components/footer";
 
 // interface MainLayoutProps {
 //     children: ReactNode
